ea288a330bc0b3def585540d268ad9168089e887
moon-1 <mhwgodnjs@naver.com>
2020-09-04 00:49:52 +0900

    Add http communication using Retrofit

---
+++ app/src/main/java/com/example/projectbut/Api/UserApi.java
@@ -0,0 +1,14 @@
+package com.example.projectbut.Api;
+
+import com.example.projectbut.entity.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface UserApi {
+
+    @GET("user")
+    Call<List<User>> getUser();
+}
---
+++ app/src/main/java/com/example/projectbut/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.projectbut.entity;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName("key")
+    int userNum;
+    String userId;
+    String password;
+    String email;
+    String phone;
+
+    public int getUserNum(){
+        return userNum;
+    }
+    public String getUserId(){
+        return userId;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPhone(){
+        return phone;
+    }
+}
